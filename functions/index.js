2dd5afa5176669c504f5096ecf76515cb8835621
ahmednet <ahmednet03@gmail.com>
2019-05-02 10:07:54 +0200

    firebase push message

--- functions/index.js
+++ functions/index.js
@@ -13,9 +13,3 @@ exports.Notification = functions.firestore.document('Notification/{projectId}').
 	    console.log('Error sending message:', error);
 	  });
 })
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,43 +11,6 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
-var cache_version = "cache-v4";
-var files = ['/', '../index.html'];
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(cache_version)
-        .then(function (cache) {
-            return cache.addAll(files)
-        })
-        .then(function (cache) {
-          return self.skipWaiting();
-        })
-        .catch(function (err) {
-            console.log("err install", err);
-        })
-    )
-})
-
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-      caches.keys().then(keys => Promise.all(
-          keys.map( key => (key === cache_version ? key:caches.delete(key)) )
-        ).then(() => {
-          console.log('remove the old cashe done');
-        }).catch(() => {
-          console.log('remove old caches faild');
-        })
-    ));
-})
-
-self.addEventListener('fetch', function(event) {
-  // console.log(event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
 messaging.setBackgroundMessageHandler(function(payload) {
   // Customize notification here
   var notificationTitle = payload.data.title;
@@ -59,13 +22,3 @@ messaging.setBackgroundMessageHandler(function(payload) {
 
   return self.registration.showNotification(notificationTitle, notificationOptions);
 });
-// self.addEventListener('push', function(event) {
-//   const title = 'Push Codelab';
-//   const options = {
-//     body: 'Yay it works.',
-//     icon: 'img/car-1.jpg',
-//     badge: 'img/car-2.jpg'
-//   };
-//   console.log(event.data.blob())
-//   event.waitUntil(self.registration.showNotification(title, options));
-// });
