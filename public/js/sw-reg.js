3e6ddbb2d65a004a96f45055bb17a5b5f50acc29
ahmednet <ahmednet03@gmail.com>
2019-05-02 14:51:47 +0200

    workbox cdn

--- public/js/sw-reg.js
+++ public/js/sw-reg.js
@@ -1,6 +1,3 @@
-var worker = document.querySelector(".worker")
-var btnYas = document.querySelector("#btn-yas")
-var btnNo = document.querySelector("#btn-no")
 if ('serviceWorker' in navigator) {
     window.addEventListener('load',function (e) {
         navigator.serviceWorker
@@ -15,34 +12,3 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
-// add to home screen
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can add to home screen
-    worker.style.display = 'block';
-});
-btnYas.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    worker.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  })
-  window.addEventListener('appinstalled', (evt) => {
-    console.dir(evt);
-  });
-  btnNo.addEventListener('click', (e) => {
-    worker.style.display = 'none'
-  })
--- public/sw.js
+++ public/sw.js
@@ -1,47 +1,30 @@
-var cache_version = "cache-v4";
-var files = ['/', '../index.html'];
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(cache_version)
-        .then(function (cache) {
-            return cache.addAll(files)
-        })
-        .then(function (cache) {
-          return self.skipWaiting();
-        })
-        .catch(function (err) {
-            console.log("err install", err);
-        })
-    )
-})
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-      caches.keys().then(keys => Promise.all(
-          keys.map( key => (key === cache_version ? key:caches.delete(key)) )
-        ).then(() => {
-          console.log('remove the old cashe done');
-        }).catch(() => {
-          console.log('remove old caches faild');
-        })
-    ));
-})
-
-self.addEventListener('fetch', function(event) {
-  // console.log(event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-// self.addEventListener('push', function(event) {
-//   const title = 'Push Codelab';
-//   const options = {
-//     body: 'Yay it works.',
-//     icon: 'img/car-1.jpg',
-//     badge: 'img/car-2.jpg'
-//   };
-//   console.log(event.data.blob())
-//   event.waitUntil(self.registration.showNotification(title, options));
-// });
+if (workbox) {
+	workbox.routing.registerRoute(
+		/car-1.jpg$/,
+		new workbox.strategies.CacheFirst({
+			 cacheName: 'img-cache',
+		})
+	);
+	workbox.routing.registerRoute(
+		'/index.html',
+		new workbox.strategies.CacheFirst({
+			 cacheName: 'html-cache',
+		})
+	);
+	workbox.routing.registerRoute(
+		/\.js$/,
+		new workbox.strategies.NetworkFirst({
+			 cacheName: 'js-cache',
+		})
+	);
+	workbox.routing.registerRoute(
+		/\.css$/,
+		new workbox.strategies.CacheFirst({
+			 cacheName: 'css-cache',
+		})
+	);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
