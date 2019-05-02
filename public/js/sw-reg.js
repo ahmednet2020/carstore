2f13f7be7f77edcf387a9a9784d13f1cbee71e60
ahmednet <ahmednet03@gmail.com>
2019-05-02 11:23:43 +0200

    caches

--- public/js/sw-reg.js
+++ public/js/sw-reg.js
@@ -1,25 +1,13 @@
-var publickey = "BLg0aCteMfSFAGtwnYmvJONFdOoR4E5AVeSqndDws7-gP3IOlDrUXPRwoi2FlhhyVtEET0wyarhcsre6UNjqIzo";
 var worker = document.querySelector(".worker")
 var btnYas = document.querySelector("#btn-yas")
 var btnNo = document.querySelector("#btn-no")
-var deferredPrompt;
-var showNotification = document.querySelector(".showNotification")
-var btnyas = document.querySelector("#btnyas")
-var btnno = document.querySelector("#btnno")
-var messaging = firebase.messaging();
-// Add the public key generated from the console here.
-messaging.usePublicVapidKey(publickey);
 if ('serviceWorker' in navigator) {
     window.addEventListener('load',function (e) {
         navigator.serviceWorker
-        .register('./firebase-messaging-sw.js')
+        .register('./sw.js')
         .then((reg) => {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', reg.scope);
-           if(Notification.permission === 'default') {
-               // initializeUI(reg);
-               firebaseRequest()
-           }
         })
         .catch((err) => {
         // registration failed :(
@@ -27,84 +15,6 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
-function firebaseRequest() {
-  showNotification.style.display = 'block'
-  btnyas.addEventListener("click", (e) => {
-      showNotification.style.display = 'none'
-      e.preventDefault()
-      messaging.requestPermission().then(function() {
-        console.log('Notification permission granted.');
-        messaging.onMessage(function(payload) {
-          console.log('Message received. ', payload);
-        })
-      }).catch(function(err) {
-        console.log('Unable to get permission to notify.', err);
-      });
-  })
-  btnno.addEventListener("click", (e) => {
-      showNotification.style.display = 'none'
-      e.preventDefault()
-  })
-}
-// get fcm sdk teken user
-messaging.getToken().then(function(teken) {
-  console.log(teken)
-
-}).catch(function (err) {
-      console.log("err" + err);
-})
-// build in function
-// function urlB64ToUint8Array(base64String) {
-//   const padding = '='.repeat((4 - base64String.length % 4) % 4);
-//   const base64 = (base64String + padding)
-//     .replace(/\-/g, '+')
-//     .replace(/_/g, '/');
-
-//   const rawData = window.atob(base64);
-//   const outputArray = new Uint8Array(rawData.length);
-
-//   for (let i = 0; i < rawData.length; ++i) {
-//     outputArray[i] = rawData.charCodeAt(i);
-//   }
-//   return outputArray;
-// }
-// // 
-// function initializeUI(swRegistration) {
-//   // Set the initial subscription value
-//   swRegistration.pushManager.getSubscription()
-//   .then(function(subscription) {
-//     //console.log(subscription)
-//     if (subscription === null) {
-//         showNotification.style.display = 'block'
-//         btnyas.addEventListener("click", (e) => {
-//             showNotification.style.display = 'none'
-//             e.preventDefault()
-//             subscribeUser(swRegistration);
-//         })
-//         btnno.addEventListener("click", (e) => {
-//             showNotification.style.display = 'none'
-//             e.preventDefault()
-//         })
-//     } else {
-//       console.log('User IS subscribed.');
-//     }
-//   });
-// }
-// //
-// function subscribeUser(swRegistration) {
-//   swRegistration.pushManager.subscribe({
-//     userVisibleOnly: true,
-//     applicationServerKey: urlB64ToUint8Array(publickey)
-//   })
-//   .then(function(subscription) {
-//     console.log('User is subscribed.');
-//   })
-//   .catch(function(err) {
-//     console.log('Failed to subscribe the user: ', err);
-//   });
-// }
-
-
 // add to home screen
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,3 @@
-importScripts('https://www.gstatic.com/firebasejs/5.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.10.1/firebase-messaging.js');
-var firebaseConfig = {
-    apiKey: "AIzaSyCAYw4YioZfWPA0QK40jGvd1Mvhr2F1iJI",
-    authDomain: "carstore-d2063.firebaseapp.com",
-    databaseURL: "https://carstore-d2063.firebaseio.com",
-    projectId: "carstore-d2063",
-    storageBucket: "carstore-d2063.appspot.com",
-    messagingSenderId: "182851594715"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
 var cache_version = "cache-v4";
 var files = ['/', '../index.html'];
 self.addEventListener('install', function (event) {
@@ -47,17 +34,6 @@ self.addEventListener('fetch', function(event) {
       return response || fetch(event.request);
     })
   );
-});
-messaging.setBackgroundMessageHandler(function(payload) {
-  // Customize notification here
-  var notificationTitle = payload.data.title;
-  var notificationOptions = {
-    body: payload.data.body,
-    icon: payload.data.icon,
-    badge: payload.data.badge
-  };
-
-  return self.registration.showNotification(notificationTitle, notificationOptions);
 });
 // self.addEventListener('push', function(event) {
 //   const title = 'Push Codelab';
